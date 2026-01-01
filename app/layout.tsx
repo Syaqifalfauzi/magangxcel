ea89771ec3f3a618d59bb41180aa97e3ea9e79b1
Syaqifalfauzi <alfauzisyaqif04@gmail.com>
2026-01-01 10:27:51 +0700

    Update layout metadata and language attribute
    Updated the title and description in the metadata for the layout. Changed the HTML language attribute from 'en' to 'id'.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,15 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Company Profile | Internship Geodesy",
+  title: "Output Hasil Magang XCEL",
   description:
-    "Company profile and internship project documentation in geodesy and geospatial mapping.",
+    "Dokumentasi dan hasil kegiatan magang di XCEL yang mencakup bidang geodesi, pemetaan, dan pengolahan data geospasial.",
+
+  icons: {
+    icon: "/sponsor/XCEL.PNG",
+    shortcut: "/sponsor/XCEL.PNG",
+    apple: "/sponsor/XCEL.PNG",
+  },
 };
 
 export default function RootLayout({
@@ -14,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="id">
       <body>
         {children}
       </body>
